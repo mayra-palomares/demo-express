89ddafb789cab786ad82eb07cac5890bfc44545c
Mayra Palomares <mayra.palomares.delacruz@gmail.com>
2023-07-17 18:21:04 +0100

    Improve error handling

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,32 +1,32 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import * as TaskService from '../services/Task.service'
 
-export const getTasks = (_req: Request, res: Response): void => {
+export const getTasks = (_req: Request, res: Response, next: NextFunction): void => {
   TaskService.getTasks().then(tasks => {
     res.send(tasks)
-  }).catch(err => console.error(err))
+  }).catch(err => next(err))
 }
 
-export const getTask = (req: Request, res: Response): void => {
+export const getTask = (req: Request, res: Response, next: NextFunction): void => {
   TaskService.getTaskById(req.params.id).then(task => {
     res.send(task)
-  }).catch(err => console.error(err))
+  }).catch(err => next(err))
 }
 
-export const addTask = (req: Request, res: Response): void => {
+export const addTask = (req: Request, res: Response, next: NextFunction): void => {
   TaskService.addTask(req.body).then(task => {
     res.send(task)
-  }).catch(err => console.error(err))
+  }).catch(err => next(err))
 }
 
-export const completeTask = (req: Request, res: Response): void => {
+export const completeTask = (req: Request, res: Response, next: NextFunction): void => {
   TaskService.completeTask(req.params.id).then(task => {
     res.send(task)
-  }).catch(err => console.error(err))
+  }).catch(err => next(err))
 }
 
-export const updateTask = (req: Request, res: Response): void => {
+export const updateTask = (req: Request, res: Response, next: NextFunction): void => {
   TaskService.updateTask(req.params.id, req.body).then(task => {
     res.send(task)
-  }).catch(err => console.error(err))
+  }).catch(err => next(err))
 }
