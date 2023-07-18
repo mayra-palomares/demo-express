db499af69621a2343e1adb4455e6f72462de1fb6
Mayra Palomares <mayra.palomares.delacruz@gmail.com>
2023-07-18 12:22:12 +0100

    Add async wrapper middleware

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,32 +1,28 @@
-import { NextFunction, Request, Response } from 'express'
+import { Request, Response } from 'express'
 import * as TaskService from '../services/Task.service'
+import asyncWrapper from '../middlewares/AsyncWrapper'
 
-export const getTasks = (_req: Request, res: Response, next: NextFunction): void => {
-  TaskService.getTasks().then(tasks => {
-    res.send(tasks)
-  }).catch(err => next(err))
-}
+export const getTasks = asyncWrapper(async (_req: Request, res: Response) => {
+  const tasks = await TaskService.getTasks()
+  res.send(tasks)
+})
 
-export const getTask = (req: Request, res: Response, next: NextFunction): void => {
-  TaskService.getTaskById(req.params.id).then(task => {
-    res.send(task)
-  }).catch(err => next(err))
-}
+export const getTask = asyncWrapper(async (req: Request, res: Response) => {
+  const task = await TaskService.getTaskById(req.params.id)
+  res.send(task)
+})
 
-export const addTask = (req: Request, res: Response, next: NextFunction): void => {
-  TaskService.addTask(req.body).then(task => {
-    res.send(task)
-  }).catch(err => next(err))
-}
+export const addTask = asyncWrapper(async (req: Request, res: Response) => {
+  const task = await TaskService.addTask(req.body)
+  res.send(task)
+})
 
-export const completeTask = (req: Request, res: Response, next: NextFunction): void => {
-  TaskService.completeTask(req.params.id).then(task => {
-    res.send(task)
-  }).catch(err => next(err))
-}
+export const completeTask = asyncWrapper(async (req: Request, res: Response) => {
+  const task = await TaskService.completeTask(req.params.id)
+  res.send(task)
+})
 
-export const updateTask = (req: Request, res: Response, next: NextFunction): void => {
-  TaskService.updateTask(req.params.id, req.body).then(task => {
-    res.send(task)
-  }).catch(err => next(err))
-}
+export const updateTask = asyncWrapper(async (req: Request, res: Response) => {
+  const task = await TaskService.updateTask(req.params.id, req.body)
+  res.send(task)
+})
