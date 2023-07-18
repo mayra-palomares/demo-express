dbbed4c0ea8c9c1e376978fb807cf78ed15131ab
Mayra Palomares <mayra.palomares.delacruz@gmail.com>
2023-07-18 12:27:20 +0100

    Improve error messages

--- src/middlewares/InvalidPath.ts
+++ src/middlewares/InvalidPath.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
 
-const InvalidPath = (_req: Request, res: Response): Response => res.status(404).send('Not found')
+const InvalidPath = (_req: Request, res: Response): Response => res.status(404).send({ success: false, message: 'Page not found' })
 
 export default InvalidPath
