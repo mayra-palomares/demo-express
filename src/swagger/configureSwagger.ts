3a5247b46b3d778e3811cc41354953b4103e0848
Mayra Palomares <mayra.palomares.delacruz@gmail.com>
2023-07-19 19:01:22 +0100

    Test swagger in /api-docs route

--- src/swagger/configureSwagger.ts
+++ src/swagger/configureSwagger.ts
@@ -7,7 +7,7 @@ import Config from '../config'
 export const configureSwaggerWithJSON = (app: Application): void => {
   const processedSwagger = JSON.stringify(swaggerJson).replace('{{API_BASE_URL}}', Config.apiUrl)
   const swaggerDocs = JSON.parse(processedSwagger)
-  app.use('/', express.static('node_modules/swagger-ui-dist/', { index: false }))
+  app.use('/api-docs', express.static('node_modules/swagger-ui-dist/', { index: false }))
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 }
 
@@ -16,4 +16,4 @@ export const configureSwaggerWithSpecs = (app: Application): void => {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 }
 
-export default configureSwaggerWithSpecs
+export default configureSwaggerWithJSON
