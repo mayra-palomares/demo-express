970610e5bf5565fca3111c27eaaa14c6ccfba50f
Mayra Palomares <mayra.palomares.delacruz@gmail.com>
2023-07-19 19:51:12 +0100

    Fix css issue

--- src/swagger/configureSwagger.ts
+++ src/swagger/configureSwagger.ts
@@ -4,10 +4,12 @@ import swaggerJson from './swagger.json'
 import swaggerSpec from './SwaggerSpec'
 import Config from '../config'
 
+const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css'
+
 export const configureSwaggerWithJSON = (app: Application): void => {
   const processedSwagger = JSON.stringify(swaggerJson).replace('{{API_BASE_URL}}', Config.apiUrl)
   const swaggerDocs = JSON.parse(processedSwagger)
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, { customCssUrl: CSS_URL }))
 }
 
 export const configureSwaggerWithSpecs = (app: Application): void => {
